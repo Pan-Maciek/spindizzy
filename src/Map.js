cc4b8a61077de3037c107832abc58466e2daa42a
koziejka <koziejka.com@gmail.com>
2017-05-20 21:03:25 +0200

    updated maps styles

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,8 @@ import Style from './Style'
 import Flat from './objects/Flat'
 import Slide from './objects/Slide'
 import Arrow from './objects/Arrow'
+import Diamond from './entities/Diamond'
+import Color from './gl/Color'
 
 const position0 = new Float32Array([0, 0, 0])
 
@@ -12,11 +14,19 @@ export default class Map {
         this.styleGroups = []
         this.elements = []
         this.selfDrawn = []
+        this.entities = []
+        this.mapBackround = false
 
         for (let group of map) {
-            let topPoints = [], sidePoints = [], botPoints = []
+            if (group.extra) {
+                this.mapBackround = new Color(...group.mapBackround)
+                continue
+            }
+
+
+            let topPoints = [], sidePoints = [], botPoints = [], lightSidePoints = []
             const style = new Style(group.style)
-            this.styleGroups.push({ botPoints, topPoints, sidePoints, style })
+            this.styleGroups.push({ botPoints, topPoints, sidePoints, style, lightSidePoints })
             for (var i = 0; i < group.elements.length; i++) {
                 const [type, ...args] = group.elements[i]
                 switch (type.toLowerCase()) {
@@ -31,12 +41,17 @@ export default class Map {
                         block.style = style
                         this.selfDrawn.push(block)
                         continue
+                    case 'diamond':
+                        var block = new Diamond(...args)
+                        block.style = style
+                        this.entities.push(block)
+                        continue
                 }
-                topPoints.push(...block.top.points)
-                sidePoints.push(...block.sides[1].points)
-                sidePoints.push(...block.sides[2].points)
-                sidePoints.push(...block.sides[3].points)
-                sidePoints.push(...block.sides[0].points)
+                topPoints.push(...block.top)
+                sidePoints.push(...block.sides[1])
+                sidePoints.push(...block.sides[0])
+                lightSidePoints.push(...block.sides[2])
+                lightSidePoints.push(...block.sides[3])
                 botPoints.push(...block.bottom)
                 this.elements.push(block)
             }
@@ -49,10 +64,19 @@ export default class Map {
             this.styleGroups[i].sidelength = this.styleGroups[i].sidePoints.length / 3
             this.styleGroups[i].botPoints = new Float32Array(this.styleGroups[i].botPoints)
             this.styleGroups[i].botlength = this.styleGroups[i].botPoints.length / 3
+            this.styleGroups[i].lightSidePoints = new Float32Array(this.styleGroups[i].lightSidePoints)
+            this.styleGroups[i].lightSidelength = this.styleGroups[i].lightSidePoints.length / 3
+        }
+    }
+
+    update(game, dt) {
+        for (var i = 0; i < this.entities.length; i++) {
+            this.entities[i].update(game, dt)
         }
     }
 
     draw(bp, gl) {
+
         for (var i = 0; i < this.styleGroups.length; i++) {
             bp.position.set(position0)
             bp.vert.set(this.styleGroups[i].topPoints)
@@ -71,12 +95,24 @@ export default class Map {
             bp.color.set(this.styleGroups[i].style.sides.stroke)
             gl.drawArrays(gl.LINES, 0, this.styleGroups[i].sidelength)
 
+
+            bp.vert.set(this.styleGroups[i].lightSidePoints)
+            
+            bp.color.set(this.styleGroups[i].style.sides.fillLight)
+            gl.drawArrays(gl.TRIANGLES, 0, this.styleGroups[i].lightSidelength)
+
+            bp.color.set(this.styleGroups[i].style.sides.stroke)
+            gl.drawArrays(gl.LINES, 0, this.styleGroups[i].lightSidelength)
+
             bp.vert.set(this.styleGroups[i].botPoints)
             gl.drawArrays(gl.LINES, 0, this.styleGroups[i].botlength)
 
         }
         for (var i = 0; i < this.selfDrawn.length; i++) {
             this.selfDrawn[i].draw(bp, gl)
+        }
+        for (var i = 0; i < this.entities.length; i++) {
+            this.entities[i].draw(bp, gl)
         }
     }
 }
